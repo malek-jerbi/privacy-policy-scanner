bf8ab3e7467b1d6321485ff401016d5e90f06c44
Malek Jerbi <malek.jerbi@gmail.Com>
2024-11-09 15:04:13 -0500

    improve prompt

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from openai import OpenAI
 from haystack.components.builders import ChatPromptBuilder
 from haystack.components.generators.chat import OpenAIChatGenerator
 from prompt_templates import main_prompt_template
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -18,9 +18,9 @@ app = FastAPI(
 class PrivacyPolicyInput(BaseModel):
     privacy_policy: str
 
+
 generator = OpenAIChatGenerator(model="gpt-4o")
 prompt_builder = ChatPromptBuilder()
-
 
 
 @app.post("/summarize_privacy_policy")
@@ -28,8 +28,8 @@ async def summarize_privacy_policy(input: PrivacyPolicyInput):
     try:
         builder = ChatPromptBuilder(template=main_prompt_template)
         prompt = builder.run(privacy_policy=input.privacy_policy)
-        response = generator.run(prompt["prompt"])["replies"][0]
-        return {"summary": response.content}
+        response = generator.run(prompt["prompt"], generation_kwargs={"response_format": {"type": "json_object"}})["replies"][0]
+        return response
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/prompt_templates.py
+++ backend/prompt_templates.py
@@ -1,4 +1,30 @@
 from haystack.dataclasses import ChatMessage
 
-main_prompt_template = [ChatMessage.from_system("You are a helpful assistant that reads privacy policies and summarizes the key points a user should be aware of. Highlight any areas that could be considered risky or dangerous.\n\n"),
-                    ChatMessage.from_user("Privacy Policy:\n\n{{ privacy_policy }}\n\nGive me a summary (with risky areas highlighted in RED):")]
+main_prompt_template = [
+    ChatMessage.from_system(
+        """
+You are a helpful assistant that reads privacy policies and summarizes the key points a user should be aware of. For any areas that could be considered risky or dangerous, please label them as "risky" in a JSON format.
+
+Format Requirements:
+
+Return the summary as a JSON object.
+Structure the JSON object as follows:
+{
+  "summary": [
+    {
+      "text": "Key point of the privacy policy.",
+      "risky": false
+    },
+    {
+      "text": "Risky or dangerous point here.",
+      "risky": true
+    }
+  ]
+}
+In each item of the "summary" array:
+text: Contains a point from the privacy policy.
+risky: Boolean that is true if the text describes a risky area and false if it doesn’t.
+"""
+    ),
+    ChatMessage.from_user("Privacy Policy:\n\n{{ privacy_policy }}\n\nJSON Summary (with risky areas labeled):"),
+]
