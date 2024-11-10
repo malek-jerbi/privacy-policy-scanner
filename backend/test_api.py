e4671dc950485b0e6eb7864c3db86a7e9de59e45
Malek Jerbi <malek.jerbi@gmail.Com>
2024-11-10 18:26:16 -0500

    fix retrieval by path

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,8 +1,12 @@
 import requests
+import os
 
 url = "http://127.0.0.1:8000/summarize_privacy_policy"
 
-with open("test_policy.txt", "r") as file:
+# Use os.path.join for cross-platform path compatibility
+file_path = os.path.join("test_policy.txt")
+
+with open(file_path, "r") as file:
     privacy_policy_text = file.read()
 
 payload = {"privacy_policy": privacy_policy_text}
