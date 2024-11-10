b1a124453ee0931c7e325c6424719e36a53c6b50
Malek Jerbi <malek.jerbi@gmail.Com>
2024-11-10 18:28:17 -0500

    specify encoding

--- backend/test_api.py
+++ backend/test_api.py
@@ -3,10 +3,9 @@ import os
 
 url = "http://127.0.0.1:8000/summarize_privacy_policy"
 
-# Use os.path.join for cross-platform path compatibility
 file_path = os.path.join("test_policy.txt")
 
-with open(file_path, "r") as file:
+with open(file_path, "r", encoding="utf-8") as file:
     privacy_policy_text = file.read()
 
 payload = {"privacy_policy": privacy_policy_text}
